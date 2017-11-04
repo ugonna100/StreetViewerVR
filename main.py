6a7acec2d8fb8f5544c5e69e0840bc4399634a74
Brian Walton <bckwalton@gmail.com>
2017-11-04 12:41:44 -0400

    Add files via upload
    Added method for determining heading/bearing given lat, long for 2 points. A -> B

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import math
 import keyfile.py as key
+
 import requests
+
 
 API_KEY = key.API_KEY
 
@@ -10,7 +13,22 @@ URL += Origin + "&destination="
 URL += Destination + "&key="
 URL += API_KEY
 
-res = requests.get(url = URL)
+res = requests.get(url=URL)
+
+# Computes the bearing in degrees from the point A(a1,a2) to
+# the point B(b1,b2). Note that A and B are given in terms of
+# screen coordinates.
+def bearing(a1, a2, b1, b2):
+    TWOPI = 6.2831853071795865
+    RAD2DEG = 57.2957795130823209
+    if (a1==b1 and a2==b2):
+        print("Origin & Destination cannot match!")
+    theta = math.atan2(b1 - a1, a2 - b2)
+    if (theta < 0.0):
+        theta += TWOPI
+    return RAD2DEG * theta
+
 if res.ok:
+#     print(bearing(100,200,1,5))
     print("POST Success")
     print(res.json())
