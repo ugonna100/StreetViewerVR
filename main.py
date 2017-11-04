fb4df82334d0367512c2cc8474c7e2e1e512f8c6
Brian Walton <bckwalton@gmail.com>
2017-11-04 12:42:58 -0400

    Almost exactly the same as previous
    Cleaning

--- main.py
+++ main.py
@@ -18,17 +18,20 @@ res = requests.get(url=URL)
 # Computes the bearing in degrees from the point A(a1,a2) to
 # the point B(b1,b2). Note that A and B are given in terms of
 # screen coordinates.
+
+
 def bearing(a1, a2, b1, b2):
     TWOPI = 6.2831853071795865
     RAD2DEG = 57.2957795130823209
-    if (a1==b1 and a2==b2):
+    if (a1 == b1 and a2 == b2):
         print("Origin & Destination cannot match!")
     theta = math.atan2(b1 - a1, a2 - b2)
     if (theta < 0.0):
         theta += TWOPI
     return RAD2DEG * theta
 
+
 if res.ok:
-#     print(bearing(100,200,1,5))
+    #     print(bearing(100,200,1,5))
     print("POST Success")
     print(res.json())
